88515784ec9b1ba83caecbb20e155cbdc3b20f3a
Marcos Ottonello <marcos@ngti.nl>
2016-02-02 22:45:50 +0100

    some refactoring

--- gcm.go
+++ gcm.go
@@ -1,57 +1,61 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"encoding/json"
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
+// GcmPushApp an application supporting GCM push
 type GcmPushApp struct {
 	apiKey string
 }
 
-type GcmPush struct {
-	To   string `json:"to"`
-	Data GcmData `json:"data"`
+type gcmPush struct {
+	To   string      `json:"to"`
+	Data gcmPushData `json:"data"`
 }
 
-type GcmData struct {
+type gcmPushData struct {
 	Message string `json:"message"`
 }
 
 func (app GcmPushApp) send(pushMessage PushMessage) {
-	specificMessage := GcmPush{pushMessage.to, GcmData{Message: pushMessage.message}}
+	specificMessage := gcmPush{pushMessage.to, gcmPushData{Message: pushMessage.message}}
 	app.sendGcm(specificMessage)
 }
 
-func (app GcmPushApp) sendGcm(gcmPush GcmPush) {
-	client := &http.Client{}
+func (app GcmPushApp) sendGcm(gcmPush gcmPush) {
+	b, _ := json.Marshal(gcmPush)
 
-	b, err := json.Marshal(gcmPush)
-
-	checkError(err)
-
-	req, err := http.NewRequest("POST", "https://android.googleapis.com/gcm/send", bytes.NewBuffer(b))
-
-	checkError(err)
-
-	req.Header.Add("Authorization", "key=" + app.apiKey)
-	req.Header.Add("Content-Type", "application/json");
-
-	fmt.Println("Sending request ", req)
-
-	resp, err := client.Do(req);
-
-	checkError(err)
-
-	fmt.Println("Success! ", resp)
+	if req, err := app.buildRequest(b); err == nil {
+		sendRequest(req)
+	} else {
+		fmt.Println("Error building request ", err)
+	}
 
 }
 
-func checkError(err error) {
-	if (err != nil) {
-		fmt.Println("Error", err)
-		panic(fmt.Sprintf("%s", err))
+func (app GcmPushApp) buildRequest(b []byte) (*http.Request, error) {
+	req, err := http.NewRequest("POST", "https://android.googleapis.com/gcm/send", bytes.NewBuffer(b))
+	req.Header.Add("Authorization", "key="+app.apiKey)
+	req.Header.Add("Content-Type", "application/json")
+
+	return req, err
+}
+
+func sendRequest(req *http.Request) {
+	client := &http.Client{}
+
+	fmt.Println("Sending request ", req)
+
+	resp, err := client.Do(req)
+
+	if err == nil {
+		fmt.Println("Success! ", resp)
+	} else {
+		fmt.Println("Error sending request", err)
 	}
+
 }
