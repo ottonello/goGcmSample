68d549b30c799c78bb1615162aa6cb92a1dd14cf
Marcos <jmottonello@gmail.com>
2016-02-03 20:49:46 +0100

    added external file for config

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type PushMessage struct {
 	to      string
 	message string
 }
 
+type Config struct {
+	GcmApiKey string `json:"gcmApiKey"`
+}
+
 func main() {
-	gcmApp := GcmPushApp{"API_KEY"}
+	config := loadConfig()
+	gcmApp := GcmPushApp{config.GcmApiKey}
 	msg := PushMessage{"/topics/global", "hello world!"}
+
+	fmt.Printf("Config %v\n", config)
 
 	gcmApp.send(msg)
 }
 
+func loadConfig() Config {
+	file, e := ioutil.ReadFile("./config.json")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	config := Config{}
+	json.Unmarshal(file, &config)
+	return config
+}
