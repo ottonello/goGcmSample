c6edd06e93ba1e447c83751895d6bcd90698b36e
Marcos <jmottonello@gmail.com>
2016-02-03 21:00:50 +0100

    refactored config loading

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
 )
 
 type PushMessage struct {
@@ -18,21 +17,32 @@ type Config struct {
 
 func main() {
 	config := loadConfig()
+	fmt.Printf("Config %v\n", config)
+
 	gcmApp := GcmPushApp{config.GcmApiKey}
 	msg := PushMessage{"/topics/global", "hello world!"}
-
-	fmt.Printf("Config %v\n", config)
 
 	gcmApp.send(msg)
 }
 
 func loadConfig() Config {
-	file, e := ioutil.ReadFile("./config.json")
+	file := readConfigFile("./config.json")
+	return unmarshalConfig(file)
+}
+
+func readConfigFile(configFile string) []byte {
+	file, e := ioutil.ReadFile(configFile)
 	if e != nil {
-		fmt.Printf("File error: %v\n", e)
-		os.Exit(1)
+		panic(fmt.Sprintf("Could not open config file %v\n", file))
 	}
+	return file
+}
+
+func unmarshalConfig(file []byte) Config {
 	config := Config{}
-	json.Unmarshal(file, &config)
+	err := json.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
 	return config
 }
